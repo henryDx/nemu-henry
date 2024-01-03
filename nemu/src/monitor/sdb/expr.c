0f8d61128198882e7489e5cc574ac55258120d63
tracer-ics2021 <tracer@njuics.org>
2024-01-03 17:51:44 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 1cbc5ef9809b48f3b3b81a883a53d766   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-91-generic       Architecture: x86-64  17:51:44 up 7 days,  3:53,  1 user,  load average: 0.29, 0.41, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -212,6 +212,7 @@ int eval(int p, int q, bool* success){
 		int t = q;
 		int mid = -1;
 		while(t>p){
+			printf("type:%d\n",tokens[t].type);
 			switch(tokens[t].type){
 				case ')':right_cnt++;
 					 printf("right++\n");

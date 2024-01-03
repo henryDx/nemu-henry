b2ce9408ba5d1688347cb845e5acbf394ea752ac
tracer-ics2021 <tracer@njuics.org>
2024-01-03 18:40:42 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 1cbc5ef9809b48f3b3b81a883a53d766   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-91-generic       Architecture: x86-64  18:40:42 up 7 days,  4:42,  1 user,  load average: 0.04, 0.12, 0.24

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -92,7 +92,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-	
+	memset(tokens[nr_token].str, 0, 32);
         switch (rules[i].token_type) {
 		case TK_NOTYPE:break;
 		case TK_HEX:;

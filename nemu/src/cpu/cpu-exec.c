dfc9b2395df7eed9b74cbd39f8e187f46665b8fe
tracer-ics2021 <tracer@njuics.org>
2024-01-25 14:31:40 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: a4f14e67507c4c7e9fe0e4e56c054b96   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-91-generic       Architecture: x86-64  14:31:40 up 1 day,  4:12,  1 user,  load average: 0.34, 0.36, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,7 +29,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
-	scan_wp();
+  if(!scan_wp()){
+	nemu_state.state = NEMU_STOP;		
+  }
 #endif
 }
 

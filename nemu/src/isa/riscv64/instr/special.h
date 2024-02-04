cb5c9e09d5f701ece22d679042fe5c0dfd8aca34
tracer-ics2021 <tracer@njuics.org>
2024-02-04 16:24:51 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  16:24:51 up  5:21,  1 user,  load average: 0.55, 0.43, 0.38

--- nemu/src/isa/riscv64/instr/special.h
+++ nemu/src/isa/riscv64/instr/special.h
@@ -2,6 +2,10 @@ def_EHelper(inv) {
   rtl_hostcall(s, HOSTCALL_INV, NULL, NULL, NULL, 0);
 }
 
+def_EHelper(jump) {
+  rtl_j(s, id_src1->imm);
+}
+
 def_EHelper(nemu_trap) {
   rtl_hostcall(s, HOSTCALL_EXIT, NULL, &gpr(10), NULL, 0); // gpr(10) is $a0
 }

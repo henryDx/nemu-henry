a26aee811f1f9b1e1581f38500d0dcba56c1887d
tracer-ics2021 <tracer@njuics.org>
2024-02-04 17:57:07 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  17:57:07 up  6:53,  1 user,  load average: 0.55, 0.38, 0.33

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jump)
+#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jump) f(ebreak)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv64/instr/special.h
+++ nemu/src/isa/riscv64/instr/special.h
@@ -4,8 +4,12 @@ def_EHelper(inv) {
 
 def_EHelper(jump) {
   vaddr_t next_pc = s->pc+id_src1->imm;
-  printf("next_pc = %lx\n", next_pc);
+  //printf("next_pc = %lx\n", next_pc);
   rtl_j(s, next_pc);
+}
+
+def_EHelper(ebreak) {
+  
 }
 
 def_EHelper(nemu_trap) {

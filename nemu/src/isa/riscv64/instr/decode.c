d7f1287dcc974b2a2eacb84de5958206203a74e4
tracer-ics2021 <tracer@njuics.org>
2024-02-05 14:36:07 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  14:36:07 up 1 day,  3:32,  1 user,  load average: 0.66, 0.48, 0.46

--- nemu/src/isa/riscv64/instr/decode.c
+++ nemu/src/isa/riscv64/instr/decode.c
@@ -55,7 +55,7 @@ static inline def_DHelper(R) {
 static inline def_DHelper(B) {
   decode_op_r(s, id_src1, s->isa.instr.b.rs1, false);
   sword_t simm = (s->isa.instr.b.simm12 << 12) |(s->isa.instr.b.simm11 << 11) | (s->isa.instr.b.simm10_5 << 5)|(s->isa.instr.b.simm4_1 << 1);
-  decode_op_i(s, id_dest, simm, false);
+  id_dest->simm = simm;
   decode_op_r(s, id_src2, s->isa.instr.b.rs2, false);
 }
 
--- nemu/src/isa/riscv64/instr/special.h
+++ nemu/src/isa/riscv64/instr/special.h
@@ -25,7 +25,7 @@ def_EHelper(nemu_trap) {
 }
 
 #define def_EHelper_branch(name, OP) def_EHelper(name) {\
-  rtl_jrelop(s, concat(RELOP_, OP), dsrc1, dsrc2, id_dest->imm);\
+  rtl_jrelop(s, concat(RELOP_, OP), dsrc1, dsrc2, s->pc+id_dest->simm);\
 }\
 
 def_EHelper_branch(beq, EQ)

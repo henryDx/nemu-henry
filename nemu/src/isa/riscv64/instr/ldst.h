3f844a35934bb47658c2d836bbaa922cb86ff028
tracer-ics2021 <tracer@njuics.org>
2024-02-05 17:17:22 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  17:17:22 up 1 day,  6:13,  1 user,  load average: 0.17, 0.28, 0.23

--- nemu/src/isa/riscv64/instr/ldst.h
+++ nemu/src/isa/riscv64/instr/ldst.h
@@ -1,31 +1,21 @@
-def_EHelper(ld) {
-  rtl_lm(s, ddest, dsrc1, id_src2->imm, 8);
-  printf("load %lx addr %lx offset %lx\n",*ddest, *dsrc1, id_src2->imm);
-}
+#define def_EHelper_load_unsinged(name, num) def_EHelper(name) {\
+  rtl_lm(s, ddest, dsrc1, id_src2->imm, num);\
+  printf("load %lx addr %lx offset %lx\n",*ddest, *dsrc1, id_src2->imm);\
+}\
 
-def_EHelper(lwu) {
-  rtl_lm(s, ddest, dsrc1, id_src2->imm, 4);
-}
+#define def_EHelper_load_singed(name, num) def_EHelper(name) {\
+  rtl_lms(s, ddest, dsrc1, id_src2->imm, num);\
+  printf("load %lx addr %lx offset %lx\n",*ddest, *dsrc1, id_src2->imm);\
+}\
 
-def_EHelper(lb) {
-  rtl_lms(s, ddest, dsrc1, id_src2->imm, 1);
-}
+def_EHelper_load_unsinged(ld, 8) 
+def_EHelper_load_unsinged(lwu, 4) 
+def_EHelper_load_unsinged(lbu, 1) 
+def_EHelper_load_unsinged(lhu, 2) 
 
-def_EHelper(lh) {
-  rtl_lms(s, ddest, dsrc1, id_src2->imm, 2);
-}
-
-def_EHelper(lw) {
-  rtl_lms(s, ddest, dsrc1, id_src2->imm, 4);
-}
-
-def_EHelper(lbu) {
-  rtl_lm(s, ddest, dsrc1, id_src2->imm, 1);
-}
-
-def_EHelper(lhu) {
-  rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
-}
+def_EHelper_load_singed(lw, 4) 
+def_EHelper_load_singed(lb, 1) 
+def_EHelper_load_singed(lh, 2) 
 
 def_EHelper(sd) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 8);

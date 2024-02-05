d70b6d43ed1beb5bd06d41a0518a753d151f80f5
tracer-ics2021 <tracer@njuics.org>
2024-02-05 12:11:39 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  12:11:39 up 1 day,  1:07,  1 user,  load average: 0.72, 0.69, 0.55

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -3,6 +3,7 @@
 
 #define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jump) f(ebreak) f(jump_reg) \
         f(lb) f(lh) f(lw) f(lbu) f(lhu) f(lwu) \
-        f(addw) f(subw) f(sllw) f(srlw) f(sraw)
+        f(addw) f(subw) f(sllw) f(srlw) f(sraw) \
+        f(add) f(sub) f(sll) f(srl) f(sra) f(slt) f(sltu) f(xor) f(or) f(and)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -25,3 +25,44 @@ def_EHelper(srlw) {
 def_EHelper(sraw) {
   rtl_sraw(s, ddest, dsrc1, dsrc2);
 }
+
+def_EHelper(add) {
+  rtl_add(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sub) {
+  rtl_sub(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(slt) {
+  //rtl_slt(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sltu) {
+  //rtl_sltu(s, ddest, dsrc1, dsrc2);
+  
+}
+
+def_EHelper(xor) {
+  rtl_xor(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(or) {
+  rtl_or(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(and) {
+  rtl_and(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sll) {
+  rtl_sll(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(srl) {
+  rtl_srl(s, ddest, dsrc1, dsrc2);
+}
+
+def_EHelper(sra) {
+  rtl_sra(s, ddest, dsrc1, dsrc2);
+}

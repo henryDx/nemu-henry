37dd0a4864923e94ab5a7f73a4c5cfdac2f54955
tracer-ics2021 <tracer@njuics.org>
2024-02-05 11:27:36 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  11:27:36 up 1 day, 23 min,  1 user,  load average: 0.33, 0.41, 0.30

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jump) f(ebreak) f(jump_reg)
+#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jump) f(ebreak) f(jump_reg) f(lb) f(lh) f(lw) f(lbu) f(lhu) f(lwu)
 
 def_all_EXEC_ID();

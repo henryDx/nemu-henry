98ba18a93e3bf7038e2ecd70b9096c9b2650531a
tracer-ics2021 <tracer@njuics.org>
2024-02-05 16:27:08 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  16:27:08 up 1 day,  5:23,  1 user,  load average: 0.65, 0.56, 0.41

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -155,6 +155,7 @@ static inline def_rtl(jr, rtlreg_t *target) {
 static inline def_rtl(jrelop, uint32_t relop,
     const rtlreg_t *src1, const rtlreg_t *src2, vaddr_t target) {
   bool is_jmp = interpret_relop(relop, *src1, *src2);
+  printf("relop:%u src1:%lx src2:%lx res:%d",relop, *src1,*src2,is_jmp);
   rtl_j(s, (is_jmp ? target : s->snpc));
 }
 #endif

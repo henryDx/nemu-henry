9c05f2bb6af7549ce644d525d4a68a554cb9da5d
tracer-ics2021 <tracer@njuics.org>
2024-02-05 15:22:06 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  15:22:06 up 1 day,  4:18,  1 user,  load average: 0.38, 0.37, 0.36

--- nemu/src/isa/riscv64/include/isa-def.h
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -29,7 +29,7 @@ typedef struct {
       uint32_t funct3    : 3;
       uint32_t rs1       : 5;
       uint32_t rs2       : 5;
-      int32_t  simm11_5  : 7;
+      uint32_t  simm11_5  : 7;
     } s;
     struct {
       uint32_t opcode1_0 : 2;
@@ -41,10 +41,10 @@ typedef struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
-      int32_t  simm19_12 : 8;
-      int32_t  simm11    : 1;
-      int32_t  simm10_1  : 10;
-      int32_t  simm20    : 1;
+      uint32_t  simm19_12 : 8;
+      uint32_t  simm11    : 1;
+      uint32_t  simm10_1  : 10;
+      uint32_t  simm20    : 1;
     } j;
     struct 
     {

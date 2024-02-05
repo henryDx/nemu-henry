93fc486c81c82ed15749611b43366a2aa8b1ac82
tracer-ics2021 <tracer@njuics.org>
2024-02-05 19:08:06 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  19:08:06 up 1 day,  8:04,  1 user,  load average: 0.55, 0.44, 0.37

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -4,7 +4,7 @@
 
 #define INSTR_LIST(f) f(auipc) f(lui) f(ld) f(inv) f(nemu_trap) f(jump) f(ebreak) f(jump_reg) \
         f(lb) f(lh) f(lw) f(lbu) f(lhu) f(lwu) \
-        f(addw) f(subw) f(sllw) f(srlw) f(sraw) f(divw) f(mulw)\
+        f(addw) f(subw) f(sllw) f(srlw) f(sraw) f(divw) f(mulw) f(remw) f(remuw) f(divuw)\
         f(add) f(sub) f(sll) f(srl) f(sra) f(slt) f(sltu) f(xor) f(or) f(and)\
         f(addi) f(subi) f(slli) f(srli) f(srai) f(slti) f(sltui) f(xori) f(ori) f(andi)\
         f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu)\

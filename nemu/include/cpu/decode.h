f093401fb3e341da1fe0b7d3f8a8cfe9611f31fe
tracer-ics2021 <tracer@njuics.org>
2024-02-06 12:44:18 +0800

    >  compile 201220000    Static hostname: henry-AG958          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8604b7dcbb4a4188ac26e9adda06371b            Boot ID: 3101b9755a704a84865351a2a7c82a6a   Operating System: Ubuntu 20.04.6 LTS             Kernel: Linux 5.15.0-92-generic       Architecture: x86-64  12:44:18 up 2 days,  1:40,  1 user,  load average: 1.00, 0.58, 0.39

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -117,14 +117,14 @@ finish:
 
 #define def_INSTR_IDTABW(pattern, id, tab, width) \
   def_INSTR_raw(pattern_decode, pattern, \
-      { concat(decode_, id)(s, width); return concat(table_, tab)(s); })
+      { concat(decode_, id)(s, width); printf("%s ",""#tab""); return concat(table_, tab)(s); })
 #define def_INSTR_IDTAB(pattern, id, tab)   def_INSTR_IDTABW(pattern, id, tab, 0)
 #define def_INSTR_TABW(pattern, tab, width) def_INSTR_IDTABW(pattern, empty, tab, width)
 #define def_INSTR_TAB(pattern, tab)         def_INSTR_IDTABW(pattern, empty, tab, 0)
 
 #define def_hex_INSTR_IDTABW(pattern, id, tab, width) \
   def_INSTR_raw(pattern_decode_hex, pattern, \
-      { concat(decode_, id)(s, width); printf("tab %s ",""#tab""); return concat(table_, tab)(s); })
+      { concat(decode_, id)(s, width); printf("%s ",""#tab""); return concat(table_, tab)(s); })
 #define def_hex_INSTR_IDTAB(pattern, id, tab)   def_hex_INSTR_IDTABW(pattern, id, tab, 0)
 #define def_hex_INSTR_TABW(pattern, tab, width) def_hex_INSTR_IDTABW(pattern, empty, tab, width)
 #define def_hex_INSTR_TAB(pattern, tab)         def_hex_INSTR_IDTABW(pattern, empty, tab, 0)
